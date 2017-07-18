53072d89604019ddfbe5dbab3c5cf9cfa23b8611
mwanjajoel <joelsilverworks@gmail.com>
2017-07-18 21:20:12 +0300

    Initial Commit

---
+++ WhatsThat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhatsThat
+//
+//  Created by Codebender on 18/07/2017.
+//  Copyright © 2017 A23 Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
